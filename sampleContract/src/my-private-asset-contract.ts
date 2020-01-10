b837b24cc8d3eb0f70ace081ac6fd79f4f74ade7
Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>
2020-01-10 14:01:02 +0000

    Final contract with appropriate tests and 100% coverage
    Signed-off-by: Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>

--- sampleContract/src/my-private-asset-contract.ts
+++ sampleContract/src/my-private-asset-contract.ts
@@ -20,7 +20,7 @@ export class MyPrivateAssetContract extends Contract {
     public async createMyPrivateAsset(ctx: Context, myPrivateAssetId: string): Promise<void> {
         const exists = await this.myPrivateAssetExists(ctx, myPrivateAssetId);
         if (exists) {
-            throw new Error(`The my asset ${myPrivateAssetId} already exists`);
+            throw new Error(`The private asset ${myPrivateAssetId} already exists`);
         }
 
         const privateAsset: any = new MyPrivateAsset();
@@ -30,7 +30,6 @@ export class MyPrivateAssetContract extends Contract {
             throw new Error(`Transient data not supplied. Please try again.`);
         }
 
-        // get the transient data and put values into the privateData object
         transientData.forEach((value, key) => {
             const dataValue: string = value.toString('utf8');
             if (key === 'privateValue') {
@@ -46,18 +45,11 @@ export class MyPrivateAssetContract extends Contract {
     public async readMyPrivateAsset(ctx: Context, myPrivateAssetId: string): Promise<string> {
         const exists = await this.myPrivateAssetExists(ctx, myPrivateAssetId);
         if (!exists) {
-            throw new Error(`The my asset ${myPrivateAssetId} does not exist`);
+            throw new Error(`The private asset ${myPrivateAssetId} does not exist`);
         }
         let privateDataString: string;
         const privateData: Buffer = await ctx.stub.getPrivateData(myCollection, myPrivateAssetId);
-        if (privateData.length > 0) {
-            privateDataString = JSON.parse(privateData.toString());
-            console.log('Private data retrieved from collection.');
-            console.log(privateDataString);
-        } else {
-            console.log('No private data with the Key: ', myPrivateAssetId);
-            return ('No private data with the Key: ' + myPrivateAssetId);
-        }
+        privateDataString = JSON.parse(privateData.toString());
         return privateDataString;
     }
 
@@ -65,7 +57,7 @@ export class MyPrivateAssetContract extends Contract {
     public async updateMyPrivateAsset(ctx: Context, myPrivateAssetId: string): Promise<void> {
         const exists = await this.myPrivateAssetExists(ctx, myPrivateAssetId);
         if (!exists) {
-            throw new Error(`The my asset ${myPrivateAssetId} does not exist`);
+            throw new Error(`The private asset ${myPrivateAssetId} does not exist`);
         }
 
         const privateAsset: any = new MyPrivateAsset();
@@ -75,7 +67,6 @@ export class MyPrivateAssetContract extends Contract {
             throw new Error(`Transient data not supplied. Please try again.`);
         }
 
-        // get the transient data and put values into the privateData object
         transientData.forEach((value, key) => {
             const dataValue: string = value.toString('utf8');
             if (key === 'privateValue') {
@@ -90,39 +81,27 @@ export class MyPrivateAssetContract extends Contract {
     public async deleteMyPrivateAsset(ctx: Context, myPrivateAssetId: string): Promise<void> {
         const exists = await this.myPrivateAssetExists(ctx, myPrivateAssetId);
         if (!exists) {
-            throw new Error(`The my asset ${myPrivateAssetId} does not exist`);
+            throw new Error(`The private asset ${myPrivateAssetId} does not exist`);
         }
         await ctx.stub.deletePrivateData(myCollection, myPrivateAssetId);
     }
 
     @Transaction(false)
     public async verifyMyPrivateAsset(ctx: Context, collection: string, myPrivateAssetId: string, hashToVerify: string): Promise<string> {
-        const org: string = ctx.clientIdentity.getMSPID();
-
-        if (org !== 'Org2MSP') {
-            throw new Error('Only the regulator can verify the asset.'); // Perhaps change message to `You can't verify your own asset`?
-        }
 
         const exists = await this.myPrivateAssetExists(ctx, myPrivateAssetId);
         if (!exists) {
-            throw new Error(`The my asset ${myPrivateAssetId} does not exist`);
+            throw new Error(`The private asset ${myPrivateAssetId} does not exist`);
         }
 
         const pdHashBytes: Buffer = await ctx.stub.getPrivateDataHash(collection, myPrivateAssetId);
-        console.log(pdHashBytes);
-        if (pdHashBytes.length > 0) {
-            console.log(pdHashBytes);
-            // got hash from the hash store
-        } else {
-            // hash doesn't exist with the given key
+        if (!pdHashBytes) {
             throw new Error('No private data hash with that Key: ' + myPrivateAssetId);
         }
 
-        //  retrieve SHA256 hash of the converted Byte array -> string from private data collection's hash store (DB)
         const actualHash: string = pdHashBytes.toString('hex');
 
         if (hashToVerify === actualHash) {
-            // verify successful
             return '\nCalculated Hash provided: \n' + hashToVerify + '\n\n             MATCHES  <----->  \n\nHash from Private Data Hash State \n' + actualHash;
         } else {
             throw new Error('Could not match the Private Data Hash State: ' + actualHash);
